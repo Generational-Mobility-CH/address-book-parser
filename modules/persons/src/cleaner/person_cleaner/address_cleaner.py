4853b87775bc29dad74ba16edd72376129494a7f
olstra <seriously251@gmail.com>
2025-07-25 14:53:44 +0200

    Refactor address_cleaner.py

--- modules/persons/src/cleaner/person_cleaner/address_cleaner.py
+++ modules/persons/src/cleaner/person_cleaner/address_cleaner.py
@@ -3,7 +3,6 @@ import re
 from modules.persons.src.models.person.address import Address
 
 
-REPLACE_DOT_AT_STRING_START = re.compile(r"^.\s*")
 STREET_NAME_AND_NUMBER = re.compile(r"(\d+[a-z]?)\s*([.a-zA-ZäöüÄÖÜ]*)")
 
 
@@ -12,21 +11,16 @@ def clean_address(address: Address) -> Address:
 
     if street_name.startswith("."):
         street_name = street_name + address.house_number
-        street_name = REPLACE_DOT_AT_STRING_START.sub("", street_name)
+        street_name = street_name.lstrip(".")
         matches = re.findall(STREET_NAME_AND_NUMBER, street_name)
 
         if matches:
-            first_element = matches[0]
-            if len(first_element) == 2:
-                a = first_element[0]
-                b = first_element[1]
-                if a[0:1:].isnumeric():
-                    house_nr = a
-                    street_name = b
-                else:
-                    house_nr = b
-                    street_name = a
+            first, second = matches[0]
+            first_starts_with_number = first[0:1:].isnumeric()
 
-                return Address(street_name=street_name, house_number=house_nr)
+            return Address(
+                street_name=second if first_starts_with_number else first,
+                house_number=first if first_starts_with_number else second,
+            )
 
     return address
