e3dcc3e7eb459db3015545f40fc04a4aa45ab627
olstra <seriously251@gmail.com>
2025-08-19 16:28:59 +0200

    feat (pages_downloader): refactor extract_book_urls.py and add .json file with all book links
    Refs: #91
    
    refactoring

--- modules/pages_downloader/src/extract_books_urls.py
+++ modules/pages_downloader/src/extract_books_urls.py
@@ -1,14 +1,12 @@
-from selenium.webdriver.chrome import webdriver
-from selenium.webdriver.common.by import By
-import time
 import json
 import re
+import time
+
+from selenium.webdriver.chrome.webdriver import WebDriver
+from selenium.webdriver.common.by import By
 
 from modules.pages_downloader.src.common.driver import get_web_driver
-from modules.pages_downloader.src.constants.paths import (
-    ALL_BOOK_LINKS_FILE,
-)
-
+from modules.pages_downloader.src.constants.paths import ALL_BOOK_LINKS_FILE
 
 RENDERING_WAIT_TIME = 5
 
@@ -19,52 +17,45 @@ overview_urls = [
     "https://dls.staatsarchiv.bs.ch/records/1231854",
 ]
 
-# TODO: store output in list for quicker access. If they're always the same - remove get_book_urls() ?
-basel_staatsarchiv_book_urls = [
-    "https://dls.staatsarchiv.bs.ch/records/79643",
-    "https://dls.staatsarchiv.bs.ch/records/79643",
-    "https://dls.staatsarchiv.bs.ch/records/79643",
-    "https://dls.staatsarchiv.bs.ch/records/79643",
-]
 
-
-def get_book_urls(urls_collection: list[str], driver: webdriver) -> list[str]:
+def get_book_urls(urls_collection: list[str], driver: WebDriver) -> list[str]:
     book_urls = []
 
     for url in urls_collection:
         driver.get(url)
         time.sleep(RENDERING_WAIT_TIME)
-        clickable_buttons = driver.find_elements(
+        expansion_panels = driver.find_elements(
             By.CSS_SELECTOR, "button.v-expansion-panel-header"
         )
 
-        for button in clickable_buttons:
-            if "Child records" in button.text:
+        for button in expansion_panels:
+            if any(
+                term in button.text
+                for term in ["Child records", "Untergeordnete Verzeichnungseinheiten"]
+            ):
                 time.sleep(1)
+                driver.execute_script("arguments[0].scrollIntoView();", button)
                 button.click()
-                print(
-                    "Clicked button!"
-                )  # TODO: this button is never clicked, remove this part?
                 break
 
         time.sleep(RENDERING_WAIT_TIME)
-
         child_elements = driver.find_elements(By.CSS_SELECTOR, "a.v-list-item--link")
 
         for element in child_elements:
             href = element.get_attribute("href")
             if "/records/" in href:
-                book_urls.extend(href)
+                book_urls.append(href)
 
     return book_urls
 
 
 def save_first_page_preview_links(
-    urls_collection: list[str], driver: webdriver
+    urls_collection: list[str], driver: WebDriver
 ) -> None:
+    urls = get_book_urls(urls_collection, driver)
     first_page_urls = []
 
-    for url in urls_collection:
+    for url in urls:
         driver.get(url)
         time.sleep(RENDERING_WAIT_TIME)
 
@@ -77,6 +68,7 @@ def save_first_page_preview_links(
         for link in all_page_links:
             if "/preview" in str(link.get_attribute("href")):
                 hyperlink = link.get_attribute("href")
+                print(hyperlink)
                 first_page_urls.append([year, hyperlink])
                 break
 
@@ -85,6 +77,7 @@ def save_first_page_preview_links(
 
 
 if __name__ == "__main__":
+    ALL_BOOK_LINKS_FILE.touch(exist_ok=True)
     demo_driver = get_web_driver()
-    save_first_page_preview_links(basel_staatsarchiv_book_urls, demo_driver)
+    save_first_page_preview_links(overview_urls, demo_driver)
     demo_driver.quit()
