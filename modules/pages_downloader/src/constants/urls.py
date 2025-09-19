f80644dc03b1fc92ac8753a6db443852a8346c87
bornready95 <niccolo.armandola@gmail.com>
2025-09-19 14:02:39 +0200

    update list of urls to include the 4 overview links for the archive's subgroups

--- modules/pages_downloader/src/constants/urls.py
+++ modules/pages_downloader/src/constants/urls.py
@@ -1,1 +1,8 @@
 PDF_DOWNLOAD_BASE_URL = "https://sos-ch-dk-2.exo.io/hfp-stabs-dls/iiif-pdf-derivatives"
+
+BASEL_ADRESS_BOOKS_OVERVIEW_URLS = [
+    "https://dls.staatsarchiv.bs.ch/records/1225908",
+    "https://dls.staatsarchiv.bs.ch/records/1225909",
+    "https://dls.staatsarchiv.bs.ch/records/1231853",
+    "https://dls.staatsarchiv.bs.ch/records/1231854",
+]
